20e93982017841667794d572e87cc641fed4fb11
Shehan <kurugamaperera2003@gmail.com>
2025-09-01 23:36:07 +0530

    Updated contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,8 @@ export default function Contact() {
     <section id="contact" className="p-10 bg-white">
       <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
       <p>Email: <a href="mailto:youremail@example.com" className="text-blue-500">kurugamaperera2003@gmail.com</a></p>
-      <p>LinkedIn: <a href="https://linkedin.com/in/yourprofile" className="text-blue-500">https://github.com/Shehanruby-67523</a></p>
-      <p>GitHub: <a href="https://github.com/yourusername" className="text-blue-500">https://github.com/Shehanruby-67523</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/shehan-perera-2642ba316/" className="text-blue-500">https://www.linkedin.com/in/shehan-perera-2642ba316/</a></p>
+      <p>GitHub: <a href="https://github.com/Shehanruby-67523" className="text-blue-500">https://github.com/Shehanruby-67523</a></p>
     </section>
   );
 }
