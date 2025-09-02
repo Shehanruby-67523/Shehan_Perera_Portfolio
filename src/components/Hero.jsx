d3e953a92bb2befe26c2d483245ffcab7d5efd19
Shehan <kurugamaperera2003@gmail.com>
2025-09-02 21:15:33 +0530

    updated her.jsx component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,6 @@ export default function Hero() {
     return (
         <section className="h-screen flex flex-col justify-center items-center text-center bg-gradient-to-r from-blue-900 to-indigo-900 text-white">
             <h1 className="text-5xl font-bold">Hi, I'm Shehan Perera👋</h1>
-            <h1 className="text-5xl font-bold text-red-500">Hello Tailwind</h1>
             <p className="mt-4 text-xl">Aspiring Full Stack Developer | React | Python | DevOps</p>
             <a href="#projects" className="mt-6 px-6 py-3 bg-blue-500 rounded-lg hover:bg-blue-600">
                 View My Work
