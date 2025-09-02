df0a60d9fea8e852eeb144da09d961e12bde1ee1
Shehan <kurugamaperera2003@gmail.com>
2025-09-02 20:23:43 +0530

    imported index.css file to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import './index.css'
 
 function App() {
   return (
